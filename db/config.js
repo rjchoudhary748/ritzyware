ebb091adfc73983d6f0fa50ded043bbbe442e1ff
rajesh choudhary <rjchoudhary748@gmail.com>
2022-10-20 00:03:29 +0530

    node js ritzyware first commit

--- db/config.js
+++ db/config.js
@@ -4,7 +4,8 @@ var con = mysql.createConnection({
   host: "b9suts7m8dvjnuwiqvdo-mysql.services.clever-cloud.com",
   user: "ugvonq0rjas1q3fm",
   password: "wFcIHgPMemB7eP20hTV",
-  database: "b9suts7m8dvjnuwiqvdo"
+  database: "b9suts7m8dvjnuwiqvdo",
+  port: '20251'
 });
 
 con.connect(function(err) {
